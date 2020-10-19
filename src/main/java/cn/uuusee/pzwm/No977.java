27193c8ea390cd8d74a78be7ba1e054bacd0abf3
“guorui” <guorui@kayak.com.cn>
2020-10-19 10:17:54 +0800

    添加每日一题

--- src/main/java/cn/uuusee/pzwm/No977.java
+++ src/main/java/cn/uuusee/pzwm/No977.java
@@ -1,7 +1,6 @@
 package cn.uuusee.pzwm;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 public class No977 {
     public int[] sortedSquares(int[] A) {
---
+++ src/test/java/cn/uuusee/pzwm/No844Test.java
@@ -0,0 +1,7 @@
+package cn.uuusee.pzwm;
+
+public class No844Test {
+    public static void main(String[] args) {
+        new No844().backspaceCompare("du###vu##v#fbtu","du###vu##v##fbtu");
+    }
+}
